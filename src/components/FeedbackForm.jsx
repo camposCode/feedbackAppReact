00f57cec0129ef34b7f11cc50861e794db1c1ac8
camposCode <info@josecampos.io>
2022-07-23 15:03:03 +0200

    addFeedback done

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ handleAdd }) => {
     const [text, setText] = useState('')
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
+    const [rating, setRating ] = useState(10)
     
     const handleTextChange = (e) => {
         if(text === ''){
@@ -20,10 +22,23 @@ const FeedbackForm = () => {
         }
         setText(e.target.value);
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10){
+            const newFeedback = {
+                text,
+                rating
+            }
+            handleAdd(newFeedback)
+            setText('')
+        }
+    }
   return (
     <Card>
-        <form>
+        <form onSubmit={ handleSubmit }>
             <h2>How do you rate your service with us?</h2>
+            <RatingSelect select={(rating) => setRating(rating)}/>
             <div className="input-group">
                 <input 
                     type="text" 
